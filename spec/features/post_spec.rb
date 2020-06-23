9a5c3aa9f0c5c783ce51b4cd6cca32d41dfd7b57
travisprol <travisjohnprol@gmail.com>
2020-06-23 18:29:55 -0400

    Done.

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+ 
+describe 'new post' do
+  it 'ensures that the form route works with the /new action' do
+    visit new_post_path
+    expect(page.status_code).to eq(200)
+  end
+end
+
+require 'rails_helper'
+ 
+describe 'new post' do
+  
+  it 'renders HTML in the /new template' do
+    visit new_post_path
+    expect(page).to have_content('Post Form')
+  end
+end
